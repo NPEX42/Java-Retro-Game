9218294a82093e2cdae89b48a3f4a08cb3d502eb
George Venn (Npex42) <npex42@gmail.com>
2020-10-07 09:02:06 +0100

    Added-LUAGFX

--- Engine/src/np/engine/lua/gfx/LuaGFX.java
+++ Engine/src/np/engine/lua/gfx/LuaGFX.java
@@ -1,8 +1,31 @@
 package np.engine.lua.gfx;
 
+import org.luaj.vm2.LuaFunction;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.OneArgFunction;
+import org.luaj.vm2.lib.TwoArgFunction;
+
 import np.engine.gfx.GraphicsEngine;
 
 public class LuaGFX {
-	GraphicsEngine gfx;
+	private static GraphicsEngine gfx;
+
+	public static void setGfx(GraphicsEngine gfx) {
+		LuaGFX.gfx = gfx;
+	}
+	
+	public static class LoadSprite extends OneArgFunction {
+
+		@Override
+		public LuaValue call(LuaValue arg) {
+			if(arg.istable()) {
+				gfx.LoadSprite(arg.get(0).checkjstring(), arg.get(0).checkjstring());
+				return LuaValue.TRUE;
+			} else {
+				return LuaValue.FALSE;
+			}
+		}
+		
+	}
 	
 }
